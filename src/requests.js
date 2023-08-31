a10e827f4ad44626b55b56de3f687584b49060bd
Volddemar4ik <volddemar4ik@gmail.com>
2023-08-31 11:42:57 +0200

    31.08.2023 11:42 delete proxy

--- src/requests.js
+++ src/requests.js
@@ -3,8 +3,8 @@ import { mainEndpoint, proxyEndpoind } from "./components/app/app"
 export async function investmentDataRequest(endpoint) {
     try {
         // console.log('request sending', endpoint)
-        // const res = await fetch(`/asset/profit?${endpoint}`)
-        const res = await fetch(`${proxyEndpoind + mainEndpoint}/asset/profit?${endpoint}`)
+        const res = await fetch(`/asset/profit?${endpoint}`)
+        // const res = await fetch(`${proxyEndpoind + mainEndpoint}/asset/profit?${endpoint}`)
         const data = await res.json()
 
         if (data.statusCode === 500) {
